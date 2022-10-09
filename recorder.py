ad2286e998c41fdd26f29fd6213c069eb297978f
hartaithan <hartaithan@gmail.com>
2022-10-09 12:02:04 +0900

    add appname and timestamp to output filename

--- recorder.py
+++ recorder.py
@@ -2,16 +2,21 @@ import cv2
 import numpy as np
 import pyautogui
 import pygetwindow as gw
-import sys
+from datetime import datetime
+from tzlocal import get_localzone
 
 window_name = "iTunes"
+
+date = datetime.now()
+localdate = date.astimezone(get_localzone()).strftime("%Y-%m-%d %H-%M-%S")
+filename = "./output/" + window_name + " " + localdate + ".avi"
 
 fourcc = cv2.VideoWriter_fourcc(*"XVID")
 fps = 60.0
 record_seconds = 5
 w = gw.getWindowsWithTitle(window_name)[0]
 w.activate()
-out = cv2.VideoWriter("./output/video.avi", fourcc, fps, tuple(w.size))
+out = cv2.VideoWriter(filename, fourcc, fps, tuple(w.size))
 
 for i in range(int(record_seconds * fps)):
     img = pyautogui.screenshot(region=(w.left, w.top, w.width, w.height))
